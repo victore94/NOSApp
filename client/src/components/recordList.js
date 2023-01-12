4f1540460e43de57933c39c64f5a55b1dad3ad93
Victor Lopez <victorel94@gmail.com>
2023-01-12 15:16:23 -0800

    changed font

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -74,7 +74,7 @@ export default function RecordList() {
   // This following section will display the table with the records of individuals.
   return (
     <div>
-      <h3>Record List</h3>
+      <h3>RECORD LIST</h3>
       <table className="table table-striped" style={{ marginTop: 20 }}>
         <thead>
           <tr>
